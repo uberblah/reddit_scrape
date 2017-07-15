01445d99e2bf6b45284e01f95d382779a52187a6
uberblah <soccermiles1@gmail.com>
2017-07-15 16:01:54 -0600

    manual_label now allows multiple field labels, continues labeling where you left off

--- manual_label.py
+++ manual_label.py
@@ -5,28 +5,43 @@ import sys
 import os
 import json
 
-def manual_label(entry):
+def manual_label(entry, field):
     print("============================================")
     print("--------------------------------------------")
     print(json.dumps(entry, indent=4))
-    raw_label = raw_input("O? > ")
-    entry["offensive"] = raw_label.lower() == "o"
+    raw_label = raw_input(field + "? (y/n) ")
+    entry[field] = raw_label.lower() == "y"
     return entry
+
+def load_already_labeled(filename):
+    try:
+        labeled_raw = []
+        with open(filename, "r") as f:
+            for line in f:
+                labeled_raw.append(json.loads(line))
+    except IOError:
+        pass
+    return {item["fullname"]: item for item in labeled_raw}
 
 def main():
     if len(sys.argv) < 3:
-        print("oh no you didn't!", file=sys.stderr)
+        print("./manual_label.py <infile.json> <outfile.json> <field>", file=sys.stderr)
         sys.exit(1)
     #undent
 
     infile = sys.argv[1]
     outfile = sys.argv[2]
+    field = sys.argv[3]
+
+    labeled = load_already_labeled(outfile)
 
     try:
         with open(infile, "r") as f, open(outfile, "a") as g:
             for line in f:
                 entry = json.loads(line)
-                print(json.dumps(manual_label(entry)), file=g)
+                if entry["fullname"] in labeled:
+                    continue
+                print(json.dumps(manual_label(entry, field)), file=g)
     except KeyboardInterrupt:
         pass # catch keyboard interrupts to prevent output truncation
 
